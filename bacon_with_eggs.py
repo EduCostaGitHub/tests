58435b55a344e64b1f74da18ba5c51ae4874614c
Eduardo Costa <educosta.costa@gmail.com>
2024-07-12 11:58:02 +0100

    unit tests 2

---
+++ bacon_with_eggs.py
@@ -0,0 +1,20 @@
+"""
+1 - Receive an  int
+2 - Know if number is multiple of 3 and 5:
+    Bacon & eggs
+3 - Know if number is multiple of 3
+    Bacon
+3 - Know if number is multiple of 5
+    eggs
+4 - Know if number is not multiple of 3 & 5
+    starving
+
+"""
+
+def bacon_with_eggs(num):
+    assert isinstance(num,int), 'number must be int'
+
+    if num %3  == 0 and num % 5==0:
+        return 'Bacon & eggs'
+    else:
+        return 'starving'
---
+++ test_bacon&eggs.py
@@ -0,0 +1,50 @@
+import unittest
+from bacon_with_eggs import bacon_with_eggs
+
+"""
+TDD - Test Driven Development
+
+RED
+Part 1 -> Make the Test e see the fail
+
+GREEN
+Part 2 -> Make the code and watch the test pass
+
+REFACTOR
+Part 3  -> Improve the code
+
+"""
+
+class TestBaconEggs(unittest.TestCase):
+
+    def test_bacon_eggs_int_parameters(self):
+        with self.assertRaises(AssertionError):
+            bacon_with_eggs('2')
+
+    def test_bacon_eggs_output_if_multiple_of_3_and_5(self):
+        inputs = (15,30,45,60)
+        output = 'Bacon & eggs'
+
+        for input in inputs:
+            with self.subTest(input=input,output=output):
+                self.assertEqual(
+                    bacon_with_eggs(input),
+                    output,
+                    msg=f'{input} did not returned {output}'
+                )
+
+    def test_bacon_eggs_output_if__NOT_multiple_of_3_and_5(self):
+        inputs = (1,2,4,7,8)
+        output = 'starving'
+
+        for input in inputs:
+            with self.subTest(input=input,output=output):
+                self.assertEqual(
+                    bacon_with_eggs(input),
+                    output,
+                    msg=f'{input} did not returned {output}'
+                )
+            
+
+unittest.main(verbosity=2)
+
